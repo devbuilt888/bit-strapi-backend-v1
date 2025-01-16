0a65c38d9c3b7a2d04103d5bda2b87387541bba5
zero <moneymavie@gmail.com>
2025-01-15 22:39:01 -0500

    Revert "New optional postgresql database fetching"
    This reverts commit eff3ed6c379b936162f370d440cda119346d0a5d.

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,43 +4,26 @@ const parse = require('pg-connection-string').parse;
 module.exports = ({ env }) => {
   if (env('NODE_ENV') === 'production') {
     const databaseUrl = env('DATABASE_URL', '');
-    if (databaseUrl) {
-      const config = parse(databaseUrl);
-      return {
-        connection: {
-          client: 'postgres',
-          connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
-            ssl: {
-              rejectUnauthorized: false
-            },
-          },
-          debug: false,
-        },
-      };
-    } else {
-      // Assume Strapi Cloud handles the database configuration
-      return {
-        connection: {
-          client: 'postgres',
-          connection: {
-            host: env('DATABASE_HOST', 'localhost'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi'),
-            user: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'strapi'),
-            ssl: {
-              rejectUnauthorized: false
-            },
-          },
-          debug: false,
-        },
-      };
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL environment variable is not set');
     }
+    const config = parse(databaseUrl);
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
+      },
+    };
   } else {
     return {
       connection: {
